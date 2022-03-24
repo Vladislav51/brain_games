543f14fbddfb5a7fe0cb4b41f78ba267085d9c19
Vladislav51 <bilinvlad@gmail.com>
2022-03-24 13:10:27 +0300

    fix lint

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,15 +4,13 @@ import prompt
 
 
 def welcome_user():
-    name=prompt.string('May I have your name? ')
+    name = prompt.string('May I have your name? ')
     print("Hello, {}!".format(name))
 
 
 def main():
     print("Welcome to the Brain Games!")
     welcome_user()
-
-
 
 
 if __name__ == '__main__':
