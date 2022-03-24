08f97d9e0eeac72b23b9104dfa2b7a99fabe37f8
Vladislav51 <bilinvlad@gmail.com>
2022-03-24 13:01:41 +0300

    fix problems

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,13 +2,18 @@
 
 import prompt
 
+
 def welcome_user():
     name=prompt.string('May I have your name? ')
     print("Hello, {}!".format(name))
-    
+
 
 def main():
     print("Welcome to the Brain Games!")
     welcome_user()
+
+
+
+
 if __name__ == '__main__':
     main()
